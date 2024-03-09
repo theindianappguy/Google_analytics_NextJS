c548ecb0f489cd3e969074516329ea9e571fbfe7
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2024-03-09 14:27:26 +0530

    new

--- task1/src/app/_app.tsx
+++ task1/src/app/_app.tsx
@@ -1,5 +1,23 @@
 import type { AppProps } from 'next/app'
- 
+import Script from 'next/script'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=G-K5STSPVGTM`} />
+
+            <Script strategy='lazyOnload'>
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-K5STSPVGTM');
+                `}
+            </Script>
+
+            <Component {...pageProps} />
+        </>
+    )
+
 }
